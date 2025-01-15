a2e8acdff6db747c5bf795b59ab534122d299512
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-01-15 11:47:55 +0000

    Using backend configuration for subnav.

--- test/hmpps/components/court-cases-release-dates/sub-navigation/subNavigation.test.ts
+++ test/hmpps/components/court-cases-release-dates/sub-navigation/subNavigation.test.ts
@@ -1,6 +1,6 @@
 import nunjucks from 'nunjucks'
 import * as cheerio from 'cheerio'
-import { DesignSystemEnvironment, SubNavigationConfig } from '../../../../../src/hmpps/@types'
+import { SubNavigationConfig, SubNavigationServices } from '../../../../../src/hmpps/@types'
 
 nunjucks.configure([
   'node_modules/govuk-frontend/dist/',
@@ -9,341 +9,113 @@ nunjucks.configure([
   __dirname,
 ])
 describe('Tests for sub navigation component', () => {
-  it.each([
-    [
-      'local' as DesignSystemEnvironment,
-      'http://localhost:3000/prisoner/ABC123/overview',
-      'http://localhost:3000/person/ABC123',
-      'http://localhost:3000/ABC123',
-      'http://localhost:3000/?prisonId=ABC123',
-      'http://localhost:3000/person/ABC123',
-      'http://localhost:3000/prisoner/ABC123/documents',
-    ],
-    [
-      'dev' as DesignSystemEnvironment,
-      'https://court-cases-release-dates-dev.hmpps.service.justice.gov.uk/prisoner/ABC123/overview',
-      'https://remand-and-sentencing-dev.hmpps.service.justice.gov.uk/person/ABC123',
-      'https://adjust-release-dates-dev.hmpps.service.justice.gov.uk/ABC123',
-      'https://calculate-release-dates-dev.hmpps.service.justice.gov.uk/?prisonId=ABC123',
-      'https://record-a-recall-dev.hmpps.service.justice.gov.uk/person/ABC123',
-      'https://court-cases-release-dates-dev.hmpps.service.justice.gov.uk/prisoner/ABC123/documents',
-    ],
-    [
-      'pre' as DesignSystemEnvironment,
-      'https://court-cases-release-dates-preprod.hmpps.service.justice.gov.uk/prisoner/ABC123/overview',
-      'https://remand-and-sentencing-preprod.hmpps.service.justice.gov.uk/person/ABC123',
-      'https://adjust-release-dates-preprod.hmpps.service.justice.gov.uk/ABC123',
-      'https://calculate-release-dates-preprod.hmpps.service.justice.gov.uk/?prisonId=ABC123',
-      'https://record-a-recall-preprod.hmpps.service.justice.gov.uk/person/ABC123',
-      'https://court-cases-release-dates-preprod.hmpps.service.justice.gov.uk/prisoner/ABC123/documents',
-    ],
-    [
-      'prod' as DesignSystemEnvironment,
-      'https://court-cases-release-dates.hmpps.service.justice.gov.uk/prisoner/ABC123/overview',
-      'https://remand-and-sentencing.hmpps.service.justice.gov.uk/person/ABC123',
-      'https://adjust-release-dates.hmpps.service.justice.gov.uk/ABC123',
-      'https://calculate-release-dates.hmpps.service.justice.gov.uk/?prisonId=ABC123',
-      'https://record-a-recall.hmpps.service.justice.gov.uk/person/ABC123',
-      'https://court-cases-release-dates.hmpps.service.justice.gov.uk/prisoner/ABC123/documents',
-    ],
-  ])(
-    'Should be able to load sub-navigation for all tabs in %s',
-    (
-      env: DesignSystemEnvironment,
-      expectedOverview: string,
-      expectedCourtCases: string,
-      expectedAdjustments: string,
-      expectedCrd: string,
-      expectedRecalls: string,
-      expectedDocuments: string,
-    ) => {
-      const config: SubNavigationConfig = {
-        environment: env,
-        prisonNumber: 'ABC123',
-        navigation: {
-          activeSubNav: 'overview',
-          overview: { enabled: true },
-          courtCases: { enabled: true },
-          adjustments: { enabled: true },
-          releaseDates: { enabled: true },
-          recalls: { enabled: true },
-          documents: { enabled: true },
-        },
-      }
-      const content = nunjucks.render('index.njk', config)
-      const links = extractLinks(content)
-
-      expect(links.Overview).toStrictEqual(expectedOverview)
-      expect(links['Court cases']).toStrictEqual(expectedCourtCases)
-      expect(links.Adjustments).toStrictEqual(expectedAdjustments)
-      expect(links['Release dates and calculations']).toStrictEqual(expectedCrd)
-      expect(links.Recalls).toStrictEqual(expectedRecalls)
-      expect(links.Documents).toStrictEqual(expectedDocuments)
-    },
-  )
-  it('should be able to give custom href for all tabs', () => {
+  it('should be for all tabs', () => {
     const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'overview',
-        overview: { enabled: true, href: '/my-overview' },
-        courtCases: { enabled: true, href: '/my-courtCases' },
-        adjustments: { enabled: true, href: '/my-adjustments' },
-        releaseDates: { enabled: true, href: '/my-releaseDates' },
-        recalls: { enabled: true, href: '/my-recalls' },
-        documents: { enabled: true, href: '/my-documents' },
-      },
+      navigation: allTabs(),
+      activeSubNav: 'overview',
     }
     const content = nunjucks.render('index.njk', config)
     const links = extractLinks(content)
-    expect(links.Overview).toStrictEqual('/my-overview')
-    expect(links['Court cases']).toStrictEqual('/my-courtCases')
-    expect(links.Adjustments).toStrictEqual('/my-adjustments')
-    expect(links['Release dates and calculations']).toStrictEqual('/my-releaseDates')
-    expect(links.Recalls).toStrictEqual('/my-recalls')
-    expect(links.Documents).toStrictEqual('/my-documents')
+    expect(links.Overview.attr('href')).toStrictEqual('http://localhost:8000/prisoner/AB1234AB/overview')
+    expect(links.Overview.attr('aria-current')).toStrictEqual('page')
+    expect(links['Court cases'].attr('href')).toStrictEqual('http://localhost:8001/person/AB1234AB')
+    expect(links.Adjustments.attr('href')).toStrictEqual('http://localhost:8002/AB1234AB')
+    expect(links.Adjustments.find('.moj-notification-badge').text()).toStrictEqual('1')
+    expect(links.Recalls.attr('href')).toStrictEqual('http://localhost:8003/person/AB1234AB')
+    expect(links['Release dates and calculations'].attr('href')).toStrictEqual(
+      'http://localhost:8004?prisonId=AB1234AB',
+    )
   })
-
-  it('enabled = true should show the tab', () => {
+  it('should be for minimal tabs', () => {
     const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'overview',
-        overview: { href: '/my-overview', enabled: true },
-        courtCases: { href: '/my-courtCases', enabled: true },
-        adjustments: { href: '/my-adjustments', enabled: true },
-        releaseDates: { href: '/my-releaseDates', enabled: true },
-        recalls: { href: '/my-recalls', enabled: true },
-        documents: { href: '/my-documents', enabled: true },
-      },
+      navigation: minimalAccess(),
+      activeSubNav: 'adjustments',
     }
     const content = nunjucks.render('index.njk', config)
-    const $ = cheerio.load(content)
     const links = extractLinks(content)
-    expect(links.Overview).toStrictEqual('/my-overview')
-    expect(links['Court cases']).toStrictEqual('/my-courtCases')
-    expect(links.Adjustments).toStrictEqual('/my-adjustments')
-    expect(links['Release dates and calculations']).toStrictEqual('/my-releaseDates')
-    expect(links.Recalls).toStrictEqual('/my-recalls')
-    expect(links.Documents).toStrictEqual('/my-documents')
-    expect($('li').length).toStrictEqual(6)
-  })
-
-  it('enabled = false should not render the overview tab', () => {
-    const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'overview',
-        overview: { href: '/my-overview', enabled: false },
-        courtCases: { href: '/my-courtCases', enabled: true },
-        adjustments: { href: '/my-adjustments', enabled: true },
-        releaseDates: { href: '/my-releaseDates', enabled: true },
-        recalls: { href: '/my-recalls', enabled: true },
-        documents: { href: '/my-documents', enabled: true },
-      },
-    }
-    const content = nunjucks.render('index.njk', config)
-    const $ = cheerio.load(content)
-    const links = extractLinks(content)
-    expect(links.Overview).toBeUndefined()
-    expect(links['Court cases']).toStrictEqual('/my-courtCases')
-    expect(links.Adjustments).toStrictEqual('/my-adjustments')
-    expect(links['Release dates and calculations']).toStrictEqual('/my-releaseDates')
-    expect(links.Recalls).toStrictEqual('/my-recalls')
-    expect(links.Documents).toStrictEqual('/my-documents')
-    expect($('li').length).toStrictEqual(5)
-  })
-
-  it('enabled = false should not render the Court cases tab', () => {
-    const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'overview',
-        overview: { href: '/my-overview', enabled: true },
-        courtCases: { href: '/my-courtCases', enabled: false },
-        adjustments: { href: '/my-adjustments', enabled: true },
-        releaseDates: { href: '/my-releaseDates', enabled: true },
-        recalls: { href: '/my-recalls', enabled: true },
-        documents: { href: '/my-documents', enabled: true },
-      },
-    }
-    const content = nunjucks.render('index.njk', config)
-    const $ = cheerio.load(content)
-    const links = extractLinks(content)
-    expect(links.Overview).toStrictEqual('/my-overview')
+    expect(links.Overview.attr('href')).toStrictEqual('http://localhost:8000/prisoner/AB1234AB/overview')
     expect(links['Court cases']).toBeUndefined()
-    expect(links.Adjustments).toStrictEqual('/my-adjustments')
-    expect(links['Release dates and calculations']).toStrictEqual('/my-releaseDates')
-    expect(links.Recalls).toStrictEqual('/my-recalls')
-    expect(links.Documents).toStrictEqual('/my-documents')
-    expect($('li').length).toStrictEqual(5)
-  })
-
-  it('enabled = false should not render the adjustments tab', () => {
-    const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'overview',
-        overview: { href: '/my-overview', enabled: true },
-        courtCases: { href: '/my-courtCases', enabled: true },
-        adjustments: { href: '/my-adjustments', enabled: false },
-        releaseDates: { href: '/my-releaseDates', enabled: true },
-        recalls: { href: '/my-recalls', enabled: true },
-        documents: { href: '/my-documents', enabled: true },
-      },
-    }
-    const content = nunjucks.render('index.njk', config)
-    const $ = cheerio.load(content)
-    const links = extractLinks(content)
-    expect(links.Overview).toStrictEqual('/my-overview')
-    expect(links['Court cases']).toStrictEqual('/my-courtCases')
-    expect(links.Adjustments).toBeUndefined()
-    expect(links['Release dates and calculations']).toStrictEqual('/my-releaseDates')
-    expect(links.Recalls).toStrictEqual('/my-recalls')
-    expect(links.Documents).toStrictEqual('/my-documents')
-    expect($('li').length).toStrictEqual(5)
-  })
-
-  it('enabled = false should not render the release dates tab', () => {
-    const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'overview',
-        overview: { href: '/my-overview', enabled: true },
-        courtCases: { href: '/my-courtCases', enabled: true },
-        adjustments: { href: '/my-adjustments', enabled: true },
-        releaseDates: { href: '/my-releaseDates', enabled: false },
-        recalls: { href: '/my-recalls', enabled: true },
-        documents: { href: '/my-documents', enabled: true },
-      },
-    }
-    const content = nunjucks.render('index.njk', config)
-    const $ = cheerio.load(content)
-    const links = extractLinks(content)
-    expect(links.Overview).toStrictEqual('/my-overview')
-    expect(links['Court cases']).toStrictEqual('/my-courtCases')
-    expect(links.Adjustments).toStrictEqual('/my-adjustments')
-    expect(links['Release dates and calculations']).toBeUndefined()
-    expect(links.Recalls).toStrictEqual('/my-recalls')
-    expect(links.Documents).toStrictEqual('/my-documents')
-    expect($('li').length).toStrictEqual(5)
-  })
-
-  it('enabled = false should not render the documents tab', () => {
-    const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'overview',
-        overview: { href: '/my-overview', enabled: true },
-        courtCases: { href: '/my-courtCases', enabled: true },
-        adjustments: { href: '/my-adjustments', enabled: true },
-        releaseDates: { href: '/my-releaseDates', enabled: true },
-        recalls: { href: '/my-recalls', enabled: true },
-        documents: { href: '/my-documents', enabled: false },
-      },
-    }
-    const content = nunjucks.render('index.njk', config)
-    const $ = cheerio.load(content)
-    const links = extractLinks(content)
-    expect(links.Overview).toStrictEqual('/my-overview')
-    expect(links['Court cases']).toStrictEqual('/my-courtCases')
-    expect(links.Adjustments).toStrictEqual('/my-adjustments')
-    expect(links['Release dates and calculations']).toStrictEqual('/my-releaseDates')
-    expect(links.Recalls).toStrictEqual('/my-recalls')
-    expect(links.Documents).toBeUndefined()
-    expect($('li').length).toStrictEqual(5)
-  })
-
-  it('enabled = false should not render the recalls tab', () => {
-    const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'overview',
-        overview: { href: '/my-overview', enabled: true },
-        courtCases: { href: '/my-courtCases', enabled: true },
-        adjustments: { href: '/my-adjustments', enabled: true },
-        releaseDates: { href: '/my-releaseDates', enabled: true },
-        recalls: { href: '/my-recalls', enabled: false },
-        documents: { href: '/my-documents', enabled: true },
-      },
-    }
-    const content = nunjucks.render('index.njk', config)
-    const $ = cheerio.load(content)
-    const links = extractLinks(content)
-    expect(links.Overview).toStrictEqual('/my-overview')
-    expect(links['Court cases']).toStrictEqual('/my-courtCases')
-    expect(links.Adjustments).toStrictEqual('/my-adjustments')
-    expect(links['Release dates and calculations']).toStrictEqual('/my-releaseDates')
+    expect(links.Adjustments.attr('href')).toStrictEqual('http://localhost:8002/AB1234AB')
+    expect(links.Adjustments.attr('aria-current')).toStrictEqual('page')
     expect(links.Recalls).toBeUndefined()
-    expect(links.Documents).toStrictEqual('/my-documents')
-    expect($('li').length).toStrictEqual(5)
-  })
-
-  it('enabled not provided should not render the tabs', () => {
-    const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'overview',
-        overview: { href: '/my-overview', enabled: true },
-        courtCases: { href: '/my-courtCases' },
-        adjustments: { href: '/my-adjustments' },
-        releaseDates: { href: '/my-releaseDates' },
-        recalls: { href: '/my-recalls' },
-        documents: { href: '/my-documents' },
-      },
-    }
-    const content = nunjucks.render('index.njk', config)
-    const $ = cheerio.load(content)
-    const links = extractLinks(content)
-    expect(links.Overview).toStrictEqual('/my-overview')
-    expect(links['Court cases']).toBeUndefined()
-    expect(links.Adjustments).toBeUndefined()
-    expect(links['Release dates and calculations']).toBeUndefined()
-    expect(links.Recalls).toBeUndefined()
-    expect(links.Documents).toBeUndefined()
-    expect($('li').length).toStrictEqual(1)
-  })
-
-  it('should highlight the active tab', () => {
-    const config: SubNavigationConfig = {
-      environment: 'dev',
-      prisonNumber: 'ABC123',
-      navigation: {
-        activeSubNav: 'release-dates',
-        releaseDates: { href: '/my-releaseDates', enabled: true },
-      },
-    }
-    const content = nunjucks.render('index.njk', config)
-    const $ = cheerio.load(content)
-    const links = $('a')
-      .map((i, element) => {
-        return { key: $(element).text(), value: $(element).attr('aria-current') }
-      })
-      .get()
-    const pages = Object.fromEntries(links.map(x => [x.key, x.value]))
-    expect(pages.Overview).toBeUndefined()
-    expect(pages['Court cases']).toBeUndefined()
-    expect(pages.Adjustments).toBeUndefined()
-    expect(pages['Release dates and calculations']).toStrictEqual('page')
-    expect(pages.Documents).toBeUndefined()
+    expect(links['Release dates and calculations'].attr('href')).toStrictEqual(
+      'http://localhost:8004?prisonId=AB1234AB',
+    )
   })
 
   function extractLinks(content: string) {
     const $ = cheerio.load(content)
     const links = $('a')
       .map((i, element) => {
-        return { key: $(element).text(), value: $(element).attr('href') }
+        return { key: $(element).contents().first().text().trim(), value: $(element) }
       })
       .get()
     return Object.fromEntries(links.map(x => [x.key, x.value]))
+  }
+
+  function minimalAccess(): SubNavigationServices {
+    return {
+      overview: {
+        href: 'http://localhost:8000/prisoner/AB1234AB/overview',
+        text: 'Overview',
+        thingsToDo: {
+          count: 0,
+        },
+      },
+      adjustments: {
+        href: 'http://localhost:8002/AB1234AB',
+        text: 'Adjustments',
+        thingsToDo: {
+          count: 0,
+        },
+      },
+      releaseDates: {
+        href: 'http://localhost:8004?prisonId=AB1234AB',
+        text: 'Release dates and calculations',
+        thingsToDo: {
+          count: 0,
+        },
+      },
+    }
+  }
+
+  function allTabs(): SubNavigationServices {
+    return {
+      overview: {
+        href: 'http://localhost:8000/prisoner/AB1234AB/overview',
+        text: 'Overview',
+        thingsToDo: {
+          count: 0,
+        },
+      },
+      courtCases: {
+        href: 'http://localhost:8001/person/AB1234AB',
+        text: 'Court cases',
+        thingsToDo: {
+          count: 0,
+        },
+      },
+      adjustments: {
+        href: 'http://localhost:8002/AB1234AB',
+        text: 'Adjustments',
+        thingsToDo: {
+          count: 1,
+        },
+      },
+      recalls: {
+        href: 'http://localhost:8003/person/AB1234AB',
+        text: 'Recalls',
+        thingsToDo: {
+          count: 0,
+        },
+      },
+      releaseDates: {
+        href: 'http://localhost:8004?prisonId=AB1234AB',
+        text: 'Release dates and calculations',
+        thingsToDo: {
+          count: 0,
+        },
+      },
+    }
   }
 })
