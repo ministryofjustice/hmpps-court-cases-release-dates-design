f07f9716dfe358ac9d8620d76f8ab7d2220339c8
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2025-05-07 12:53:29 +0100

    ADJST-1041 Move Accessibility Statements from CRDS to CCARD

--- test/hmpps/components/court-cases-release-dates/footer/footer.test.ts
+++ test/hmpps/components/court-cases-release-dates/footer/footer.test.ts
@@ -82,7 +82,7 @@ describe('Tests for footer component', () => {
     const $ = cheerio.load(content)
     expect($('#footer-accessibility-statement-link').text().trim()).toStrictEqual('Accessibility')
     expect($('#footer-accessibility-statement-link').attr('href')).toStrictEqual(
-      'https://calculate-release-dates-dev.hmpps.service.justice.gov.uk/accessibility',
+      'https://court-cases-release-dates-dev.hmpps.service.justice.gov.uk/accessibility',
     )
   })
 
