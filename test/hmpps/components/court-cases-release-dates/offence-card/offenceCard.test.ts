b46107e51de77a1dfe62a46b1263090f2b80de35
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2026-03-31 15:32:52 +0100

    RASS-1966-adding test to check empty aggravating factors

--- test/hmpps/components/court-cases-release-dates/offence-card/offenceCard.test.ts
+++ test/hmpps/components/court-cases-release-dates/offence-card/offenceCard.test.ts
@@ -128,6 +128,87 @@ describe('Tests for offence card component', () => {
     expect(extractOffenceCard(content)).toStrictEqual(expectedOffenceCard)
   })
 
+  it('does not show aggravating factors when not provided', () => {
+    const offenceCodeConfig: OffenceCardConfig = {
+      offenceCode: 'OFFENCECODE',
+      offenceName: 'An Offence Name',
+      offenceStartDate: '27 06 2024',
+      offenceEndDate: '27 08 2024',
+      outcome: 'Imprisonment',
+      countNumber: '1',
+      convictionDate: '12 09 2024',
+      terrorRelated: false,
+      isSentenced: true,
+      periodLengths: [
+        {
+          description: 'Sentence length',
+          years: '1',
+          months: '2',
+          weeks: '3',
+          days: '4',
+          periodOrder: ['years', 'months', 'weeks', 'days'],
+          legacyData: undefined,
+          periodLengthType: 'SENTENCE_LENGTH',
+        },
+      ],
+      sentenceType: 'SDS (Standard Determinate Sentence)',
+      actions: {
+        items: [
+          {
+            text: 'Edit',
+            href: '/edit',
+          },
+        ],
+      },
+    }
+
+    const content = nunjucks.render('index.njk', { offenceCodeConfig })
+    const offenceCard = extractOffenceCard(content)
+
+    expect(offenceCard.offenceSummary).not.toHaveProperty('Aggravating factors')
+  })
+
+  it('does not show aggravating factors when an empty array is provided', () => {
+    const offenceCodeConfig: OffenceCardConfig = {
+      offenceCode: 'OFFENCECODE',
+      offenceName: 'An Offence Name',
+      offenceStartDate: '27 06 2024',
+      offenceEndDate: '27 08 2024',
+      outcome: 'Imprisonment',
+      countNumber: '1',
+      convictionDate: '12 09 2024',
+      terrorRelated: false,
+      isSentenced: true,
+      periodLengths: [
+        {
+          description: 'Sentence length',
+          years: '1',
+          months: '2',
+          weeks: '3',
+          days: '4',
+          periodOrder: ['years', 'months', 'weeks', 'days'],
+          legacyData: undefined,
+          periodLengthType: 'SENTENCE_LENGTH',
+        },
+      ],
+      sentenceType: 'SDS (Standard Determinate Sentence)',
+      aggravatingFactors: [],
+      actions: {
+        items: [
+          {
+            text: 'Edit',
+            href: '/edit',
+          },
+        ],
+      },
+    }
+
+    const content = nunjucks.render('index.njk', { offenceCodeConfig })
+    const offenceCard = extractOffenceCard(content)
+
+    expect(offenceCard.offenceSummary).not.toHaveProperty('Aggravating factors')
+  })
+
   it('can load offence code with correctly formatted fields where all optional ones do not show', () => {
     const offenceCodeConfig: OffenceCardConfig = {
       offenceCode: 'OFFENCECODE',
